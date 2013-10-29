43be979d0fe9e748eb822dec9efe3389baac59cd
Julia Hopkins <jfhopkins6@gmail.com>
2013-10-29 17:41:59 -0400

    exercise 2

---
+++ bowling.rb
@@ -0,0 +1,22 @@
+class Game
+
+  def score(something)
+    scores_array = something
+    total_score = 0
+    bonus = 0
+    scores_array.each {|x| total_score += x}
+    perfect_game = []
+    perfect_game = scores_array.select {|x| x != 10}
+    if perfect_game == []
+      return total_score = 300
+    end
+    scores_array.each_with_index do |x, i| 
+      if x == 10
+        bonus += scores_array[i+1] + scores_array[i+2]
+      end
+    end
+    total_score + bonus
+
+  end
+
+end
--- changer.rb
+++ changer.rb
@@ -2,16 +2,23 @@ class Changer
 
   def self.make_change(money)
     change_array = []
-    quarters = money / 25
-    quarters.times {|i| change_array << 25}
-    dimes = (money % 25) / 10
-    dimes.times {|i| change_array << 10}
-    nickels = ((money % 25) % 10) / 5
-    nickels.times {|i| change_array << 5}
+    quarters(money).times {|i| change_array << 25}
+    dimes(money % 25).times {|i| change_array << 10}
+    nickels((money % 25) % 10).times {|i| change_array << 5}
     pennies = ((money % 25) % 10) % 5
     pennies.times{|x| change_array << 1}
     change_array
   end
 
+  def self.quarters(total_change)
+    total_change/25
+  end
 
+  def self.dimes(change_left)
+    change_left / 10
+  end
+
+  def self.nickels(after_dimes)
+    after_dimes / 5
+  end
 end
